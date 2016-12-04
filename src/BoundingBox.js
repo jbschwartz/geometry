4ad2f71a8621bf5d7442e594c78775c5214c64f6
James Schwartz <james@schwartz.engineer>
2016-12-04 17:28:18 -0500

    Implement BoundingBox width and height.

--- src/BoundingBox.js
+++ src/BoundingBox.js
@@ -3,4 +3,12 @@ export default class BoundingBox {
     this.min = min || null;
     this.max = max || null;
   }
+
+  get width() {
+    return (this.max.x - this.min.x);
+  }
+
+  get height() {
+    return (this.max.y - this.min.y);
+  }
 }
