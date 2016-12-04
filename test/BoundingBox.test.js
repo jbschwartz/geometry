8ac31af853dbd82d071a0e74fc885831374cd85f
James Schwartz <james@schwartz.engineer>
2016-12-04 17:24:02 -0500

    Test BoundingBox for width and height properties.

--- test/BoundingBox.test.js
+++ test/BoundingBox.test.js
@@ -2,10 +2,18 @@ import BoundingBox from '../src/BoundingBox'
 import Point from '../src/Point'
 
 describe('BoundingBox', () => {
-  const box = new BoundingBox();
+  const box = new BoundingBox(new Point(0, 0), new Point(10, 20));
 
   it('contains min and max variables', () => {
     expect(box.min).toBeDefined();
     expect(box.max).toBeDefined();
+  });
+
+  it('calculates width', () => {
+    expect(box.width).toBe(10);
+  });
+
+  it('calculates height', () => {
+    expect(box.height).toBe(20);
   });
 });
