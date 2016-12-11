782cc1a688612ede839f2f88e9564c7b15bc92a5
James Schwartz <james@schwartz.engineer>
2016-12-11 17:07:02 -0500

    Add KDTree tests.

---
+++ test/KDTree.test.js
@@ -0,0 +1,53 @@
+import KDTree from '../src/KDTree'
+import Point from '../src/Point'
+
+describe('KDTree', () => {
+  const points = [
+    new Point(-1, 1),
+    new Point(-1, 0),
+    new Point(-1, -1),
+    new Point(0.5, 0),
+    new Point(1, 1),
+    new Point(1, 0),
+    new Point(1, -1)
+  ]
+
+  const tree = new KDTree(points);
+
+  describe('branch', () => {
+    it('splits in X by default', () => {
+
+    });
+  })
+
+  it('contains a root node', () => {
+    expect(tree.root).toBeDefined();
+    expect(tree.root.equals(points[3])).toBeTruthy();
+  });
+
+  it('correctly creates a tree using median search', () => {
+    expect(tree.root.left.equals(new Point(-1, 0))).toBeTruthy();
+    expect(tree.root.left.left.equals(new Point(-1, -1))).toBeTruthy();
+    expect(tree.root.left.right.equals(new Point(-1, 1))).toBeTruthy();
+
+    expect(tree.root.right.equals(new Point(1, 0))).toBeTruthy();
+    expect(tree.root.right.left.equals(new Point(1, -1))).toBeTruthy();
+    expect(tree.root.right.right.equals(new Point(1, 1))).toBeTruthy();
+  });
+
+  describe('leaf nodes', () => {
+    it('have no left child', () => {
+      expect(tree.root.right.left.left).toBeUndefined();
+      expect(tree.root.right.right.left).toBeUndefined();
+      expect(tree.root.left.left.left).toBeUndefined();
+      expect(tree.root.left.right.left).toBeUndefined();
+    });
+    it('have no right child', () => {
+      expect(tree.root.right.left.right).toBeUndefined();
+      expect(tree.root.right.right.right).toBeUndefined();
+      expect(tree.root.left.left.right).toBeUndefined();
+      expect(tree.root.left.right.right).toBeUndefined();
+    });
+  })
+
+});
