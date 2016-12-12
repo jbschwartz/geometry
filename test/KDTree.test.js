4b01d5954b1ab15881dc0c95365b8ecd623b7282
James Schwartz <james@schwartz.engineer>
2016-12-11 21:34:45 -0500

    Reorganize KD Tree tests.

--- test/KDTree.test.js
+++ test/KDTree.test.js
@@ -30,31 +30,41 @@ describe('KDTree', () => {
     tree = new KDTree(points);
   })
 
-  it('assigns a split direction to each node', () => {
-    traverseAll(tree, (node) => {
-      expect(node.direction).toBeDefined();
-    })
-  });
-
-  describe('branch', () => {
-    it('splits in X by default', () => {
-      expect(tree.root.direction).toEqual(AXES.X);
-    });
-  })
-
   it('contains a root node', () => {
     expect(tree.root).toBeDefined();
     expect(tree.root.equals(points[3])).toBeTruthy();
   });
+  
+  describe('add', () => {
+    it('adds a point to tree', () => {
+      const point = new Point(-2, 1);
+      tree.add(point);
 
-  it('correctly creates a tree using median search', () => {
-    expect(tree.root.left.equals(new Point(-1, 0))).toBeTruthy();
-    expect(tree.root.left.left.equals(new Point(-1, -1))).toBeTruthy();
-    expect(tree.root.left.right.equals(new Point(-1, 1))).toBeTruthy();
+      expect(tree.root.left.right.left).toBeDefined();
+      expect(tree.root.left.right.left.equals(point)).toBeTruthy();
+    });
+  });
 
-    expect(tree.root.right.equals(new Point(1, 0))).toBeTruthy();
-    expect(tree.root.right.left.equals(new Point(1, -1))).toBeTruthy();
-    expect(tree.root.right.right.equals(new Point(1, 1))).toBeTruthy();
+  describe('branch', () => {
+    it('assigns a split direction to each node', () => {
+      traverseAll(tree, (node) => {
+        expect(node.direction).toBeDefined();
+      })
+    });
+
+    it('splits in X by default', () => {
+      expect(tree.root.direction).toEqual(AXES.X);
+    });
+
+    it('correctly creates a tree using median search', () => {
+      expect(tree.root.left.equals(new Point(-1, 0))).toBeTruthy();
+      expect(tree.root.left.left.equals(new Point(-1, -1))).toBeTruthy();
+      expect(tree.root.left.right.equals(new Point(-1, 1))).toBeTruthy();
+
+      expect(tree.root.right.equals(new Point(1, 0))).toBeTruthy();
+      expect(tree.root.right.left.equals(new Point(1, -1))).toBeTruthy();
+      expect(tree.root.right.right.equals(new Point(1, 1))).toBeTruthy();
+    });
   });
 
   describe('leaf nodes', () => {
@@ -64,21 +74,12 @@ describe('KDTree', () => {
       expect(tree.root.left.left.left).toBeUndefined();
       expect(tree.root.left.right.left).toBeUndefined();
     });
+
     it('have no right child', () => {
       expect(tree.root.right.left.right).toBeUndefined();
       expect(tree.root.right.right.right).toBeUndefined();
       expect(tree.root.left.left.right).toBeUndefined();
       expect(tree.root.left.right.right).toBeUndefined();
-    });
-  });
-
-  describe('add', () => {
-    it('adds a point to tree', () => {
-      const point = new Point(-2, 1);
-      tree.add(point);
-
-      expect(tree.root.left.right.left).toBeDefined();
-      expect(tree.root.left.right.left.equals(point)).toBeTruthy();
     });
   });
 });
