f1fd2ef0573be317799008ab4e2ee0b714167f22
James Schwartz <james@schwartz.engineer>
2016-12-15 22:20:30 -0500

    Export Edge and Polygon classes.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import BoundingBox from './BoundingBox'
+import Edge from './Edge'
 import KDTree from './KDTree'
 import Point from './Point'
+import Polygon from './Polygon'
 
 export {
   BoundingBox,
+  Edge,
   KDTree,
-  Point
+  Point,
+  Polygon
 };
