54f32661e16592450823142daa1347cc8bae89ef
James Schwartz <james@schwartz.engineer>
2016-12-15 22:18:22 -0500

    Add Polygon contains function implementation.

--- src/Polygon.js
+++ src/Polygon.js
@@ -1,9 +1,31 @@
+import Edge from './Edge'
+
 export default class Polygon {
   constructor(vertices) {
     this.vertices = vertices;
   }
 
+  // Implements winding number test
   contains(point) {
-    
+    if(!point) return false;
+    // Iterate over _edges_
+    let windingNumber = 0;
+    for(let i = 0; i < this.vertices.length; ++i) {
+      const start = this.vertices[i];
+      const end = this.vertices[i + 1] || this.vertices[0];
+      const edge = new Edge(start, end);
+
+      if(edge.crosses(point.y)) {
+        if(edge.isLeftOf(point)) {
+          // Downward edge
+          windingNumber++;
+        } else {
+          // Upward edge
+          windingNumber--;
+        }
+      }
+    }
+
+    return windingNumber > 0;
   }
 }
