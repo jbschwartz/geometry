2a2e9126084397b341d9197ba13660d161574a24
James Schwartz <james@schwartz.engineer>
2016-12-15 22:17:16 -0500

    Add tests for isLeftOf function.

--- test/Edge.test.js
+++ test/Edge.test.js
@@ -4,6 +4,27 @@ import Point from '../src/Point'
 describe('Edge', () => {
   const upEdge = new Edge(new Point(0, 0), new Point(1, 1));
   const downEdge =  new Edge(new Point(1, 1), new Point(0, 0));
+
+  describe('isLeftOf', () => {
+    const p1 = new Point(2, 0);
+    const p2 = new Point(-2, 0);
+    it('returns true when edge is to the left of point', () => {
+      expect(upEdge.isLeftOf(p1)).toBeTruthy();
+      expect(downEdge.isLeftOf(p2)).toBeTruthy();
+    });
+
+    it('returns false when edge is to the right of point', () => {
+      expect(upEdge.isLeftOf(p2)).toBeFalsy();
+      expect(downEdge.isLeftOf(p1)).toBeFalsy();
+    });
+
+    it('returns false when the point is on the edge', () => {
+      const point = new Point(0, 0)
+      expect(upEdge.isLeftOf(point)).toBeFalsy();
+      expect(downEdge.isLeftOf(point)).toBeFalsy();
+    });
+  })
+
   describe('crosses', () => {
     it('returns true when edge crosses y-value', () => {
       expect(upEdge.crosses(0.5)).toBeTruthy();
