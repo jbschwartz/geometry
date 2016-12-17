60be8f20e134a9bcb7d363b3a1fc03f9f11399d6
James Schwartz <james@schwartz.engineer>
2016-12-17 17:54:53 -0500

    Implement Edge mipoint, length, and lengthSq functions.

--- src/Edge.js
+++ src/Edge.js
@@ -1,3 +1,5 @@
+import Point from './Point'
+
 export default class Edge {
   constructor(start, end) {
     [this.start, this.end] = [start, end];
@@ -15,5 +17,21 @@ export default class Edge {
     const [x1, y1] = [this.start.x, this.start.y];
     const [x2, y2] = [this.end.x, this.end.y];
     return Math.sign((x2 - x1) * (point.y - y1) - (y2 - y1) * (point.x - x1)) < 0;
+  }
+
+  midpoint() {
+    const average = (a, b) => (a + b) / 2;
+    return new Point(
+      average(this.start.x, this.end.x),
+      average(this.start.y, this.end.y)
+    );
+  }
+
+  length() {
+    return this.start.distanceTo(this.end);
+  }
+
+  lengthSq() {
+    return this.start.distanceToSq(this.end);
   }
 }
