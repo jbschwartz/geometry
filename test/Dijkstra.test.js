0423a990a0b5a929e88c68301e1805526090e54f
James Schwartz <james@schwartz.engineer>
2016-12-17 13:24:36 -0500

    Add Dijkstra shortest path graph algorithm tests.

---
+++ test/Dijkstra.test.js
@@ -0,0 +1,35 @@
+import Dijkstra from '../src/Dijkstra'
+import Point from '../src/Point'
+
+describe('Dijkstra', () => {
+  const [A, B, C, D, E, F, G] = [
+    new Point(0, 0),
+    new Point(5, 0),
+    new Point(10, 0),
+    new Point(8, 2),
+    new Point(8, 0),
+    new Point(8, -4),
+    new Point(8, -2)
+  ]
+
+  A.connections = [B, C];
+  B.connections = [A, D, E];
+  C.connections = [A, E];
+  D.connections = [B, E, F];
+  E.connections = [B, C, D];
+  F.connections = [D, G];
+  G.connections = [E, F];
+
+  const points = [A, B, C, D, E, F, G];
+  const djikstra = new Dijkstra(points, A);
+
+  describe('shortestPath', () => {
+    it('returns an array with the shortest path through the graph', () => {
+      const path = expect(djikstra.shortestPath(F));
+      const expected = [A, B, E, G, F];
+      for(let i = 0; i < path.length; ++i) {
+        expect(path[i].equals(expected[i])).toBeTruthy();
+      }
+    });
+  });
+});
