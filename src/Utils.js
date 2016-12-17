bdf2530c232b49babe26c0606da9afb2da4ce80a
James Schwartz <james@schwartz.engineer>
2016-12-17 15:52:31 -0500

    Add randomPoint function implementation.

--- src/Utils.js
+++ src/Utils.js
@@ -9,4 +9,18 @@ function random(min = 0, max = 1, generator = Math.random) {
   return generator() * (max - min) + min;
 }
 
-export { random }
+const DEFAULT_BOX = new BoundingBox(
+  new Point(0, 0),
+  new Point(1, 1)
+);
+
+function randomPoint(boundingBox = DEFAULT_BOX, generator = random) {
+  boundingBox = boundingBox || DEFAULT_BOX;
+  
+  return new Point(
+    generator(boundingBox.min.x, boundingBox.max.x),
+    generator(boundingBox.min.y, boundingBox.max.y)
+  );
+}
+
+export { random, randomPoint }
