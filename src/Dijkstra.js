07d020d0447835412f65ec3fb0fe00464ce30a56
James Schwartz <james@schwartz.engineer>
2016-12-17 13:24:48 -0500

    Add Dijkstra algorithm implementation.

---
+++ src/Dijkstra.js
@@ -0,0 +1,57 @@
+export default class Dijkstra {
+	constructor(vertices, initial) {
+		this.vertices = vertices;
+		this.setInitial(initial);
+	}
+
+	setInitial(point) {
+		// New initial point => invalidate the current graph
+		this.isValid = false;
+		this.initial = point;
+
+	}
+
+	shortestPath(goal) {
+		if(!this.initial) throw "Initial point is required";
+		if(!this.isValid) this.build();
+
+		let current = goal;
+		let path = [];
+
+		do {
+			path.push(current);
+			current = current.previous;
+		}
+		while(current && current.distance <= 0);
+
+		return path.reverse();
+	}
+
+	initializeDistances() {
+		this.vertices.forEach(vertex => { vertex.distance = Number.INFINITY });
+		this.initial.distance = 0;
+	}
+
+	build() {
+		this.initializeDistances();
+
+		let fringe = [];
+		let current = this.initial;
+		do {
+			current.connections.forEach(neighbor => {
+				const newDistance = current.distance + current.distanceToSq(neighbor);
+				if(newDistance < neighbor.distance) {
+					neighbor.distance = newDistance;
+					neighbor.previous = current;
+					fringe.push(neighbor);
+				}
+			})
+
+			// Take the shortest node from the fringe list
+			fringe.sort((a, b) => a.distance - b.distance);
+			current = fringe.splice(0, 1)[0];
+		} while(fringe.length > 0);
+
+		this.isValid = true;
+	}
+}
