8499298264828ac7eea488336bb09ebf29ae2c7d
James Schwartz <james@schwartz.engineer>
2016-12-17 15:53:45 -0500

    Whitespace.

--- test/Point.test.js
+++ test/Point.test.js
@@ -14,6 +14,7 @@ describe('Point', () => {
       expect(p1.distanceTo(p2)).toBe(2);
       expect(p3.distanceTo(p1)).toBe(Math.sqrt(2));
     });
+    
     it('is independent of order', () => {
       expect(p1.distanceTo(p3)).toBe(p3.distanceTo(p1));
     });
@@ -24,6 +25,7 @@ describe('Point', () => {
       expect(p1.distanceToSq(p2)).toBe(4);
       expect(p3.distanceToSq(p1)).toBe(2);
     });
+
     it('is independent of order', () => {
       expect(p1.distanceToSq(p3)).toBe(p3.distanceToSq(p1));
     });
@@ -34,14 +36,17 @@ describe('Point', () => {
       expect(p1.equals(p4)).toBeTruthy();
       expect(p1.equals(p2)).toBeFalsy();
     });
+
     it('ignores order', () => {
       expect(p4.equals(p1)).toBeTruthy();
       expect(p2.equals(p1)).toBeFalsy();
     });
+
     it('ignores changes to javascript object', () => {
       p1.someProperty = 'isNowDefined';
       expect(p1.equals(p4)).toBeTruthy();
     });
+
     it('returns false for comparison with a malformed Point', () => {
       const malformed = new Point(NaN, NaN);
       expect(p1.equals(malformed)).toBeFalsy();
