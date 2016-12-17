c72c3ba9e61c1db4949265921307bd9f33186446
James Schwartz <james@schwartz.engineer>
2016-12-17 13:31:03 -0500

    Export Dijkstra class.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import BoundingBox from './BoundingBox'
+import Dijkstra from './Dijkstra'
 import Edge from './Edge'
 import KDTree from './KDTree'
 import Point from './Point'
@@ -6,6 +7,7 @@ import Polygon from './Polygon'
 
 export {
   BoundingBox,
+  Dijkstra,
   Edge,
   KDTree,
   Point,
