14cc118ebefe8194007dd23b940f7bf75160e4e9
James Schwartz <james@schwartz.engineer>
2016-12-18 12:49:24 -0500

    Tabs to spaces.

--- src/Dijkstra.js
+++ src/Dijkstra.js
@@ -1,56 +1,56 @@
 export default class Dijkstra {
-	constructor(vertices, initial) {
-		this.vertices = vertices;
-		this.setInitial(initial);
-	}
+  constructor(vertices, initial) {
+    this.vertices = vertices;
+    this.setInitial(initial);
+  }
 
-	setInitial(point) {
-		// New initial point => invalidate the current graph
-		this.isValid = false;
-		this.initial = point;
-	}
+  setInitial(point) {
+    // New initial point => invalidate the current graph
+    this.isValid = false;
+    this.initial = point;
+  }
 
-	shortestPath(goal) {
-		if(!this.initial) throw "Initial point is required";
-		if(!this.isValid) this.build();
+  shortestPath(goal) {
+    if(!this.initial) throw "Initial point is required";
+    if(!this.isValid) this.build();
 
-		let current = goal;
-		let path = [];
+    let current = goal;
+    let path = [];
 
-		do {
-			path.push(current);
-			current = current.previous;
-		}
-		while(current && current.distance >= 0);
+    do {
+      path.push(current);
+      current = current.previous;
+    }
+    while(current && current.distance >= 0);
 
-		return path.reverse();
-	}
+    return path.reverse();
+  }
 
-	initializeDistances() {
-		this.vertices.forEach(vertex => { vertex.distance = Infinity });
-		this.initial.distance = 0;
-	}
+  initializeDistances() {
+    this.vertices.forEach(vertex => { vertex.distance = Infinity });
+    this.initial.distance = 0;
+  }
 
-	build() {
-		this.initializeDistances();
+  build() {
+    this.initializeDistances();
 
-		let fringe = [];
-		let current = this.initial;
-		do {
-			current.connections.forEach(neighbor => {
-				const newDistance = current.distance + current.distanceTo(neighbor);
-				if(newDistance < neighbor.distance) {
-					neighbor.distance = newDistance;
-					neighbor.previous = current;
-					if(fringe.indexOf(neighbor) === -1) fringe.push(neighbor);
-				}
-			})
+    let fringe = [];
+    let current = this.initial;
+    do {
+      current.connections.forEach(neighbor => {
+        const newDistance = current.distance + current.distanceTo(neighbor);
+        if(newDistance < neighbor.distance) {
+          neighbor.distance = newDistance;
+          neighbor.previous = current;
+          if(fringe.indexOf(neighbor) === -1) fringe.push(neighbor);
+        }
+      })
 
-			// Take the shortest node from the fringe list
-			fringe.sort((a, b) => a.distance - b.distance);
-			current = fringe.splice(0, 1)[0];
-		} while(current && fringe.length >= 0);
+      // Take the shortest node from the fringe list
+      fringe.sort((a, b) => a.distance - b.distance);
+      current = fringe.splice(0, 1)[0];
+    } while(current && fringe.length >= 0);
 
-		this.isValid = true;
-	}
+    this.isValid = true;
+  }
 }
